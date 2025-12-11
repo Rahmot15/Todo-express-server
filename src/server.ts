70994a38a33f037ca9b450c2509bc716f4f8d627
Rahmot15 <mdrahmatulla666@gmail.com>
2025-12-11 18:17:20 +0600

    first commit

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import { Request, Response } from "express"
+
+const express = require('express')
+const app = express()
+const port = 5000
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World.......This is a test!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
