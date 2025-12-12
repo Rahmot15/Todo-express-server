e47c2cde03d380db7f17d0c14ac2a523faab9322
Rahmot15 <mdrahmatulla666@gmail.com>
2025-12-12 06:45:54 +0600

    get single todo

--- src/server.ts
+++ src/server.ts
@@ -171,6 +171,25 @@ app.get("/todos", async(req: Request, res: Response) => {
   }
 })
 
+
+// get single todo
+app.get("/todos/:id", async(req: Request, res: Response) => {
+  try {
+    const result = await pool.query(
+      `SELECT * FROM users WHERE id = $1`, [req.params.id]
+    )
+
+    if(result.rows.length === 0) {
+      return sendResponse(res, 404, false, "user not found");
+    } else {
+      return sendResponse(res, 200, true, "single todo fetch successfully", result.rows[0]);
+    }
+  } catch (error: any) {
+    return sendResponse(res, 500, false, error.message);
+  }
+})
+
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
